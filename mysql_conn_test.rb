c1eb7d6286276dd28c004709e90bb952d5bf694b
kryptykfysh <kryptykfysh@kryptykfysh.co.uk>
2012-09-07 14:27:41 +1000

    Update mysql_conn_test.rb

--- mysql_conn_test.rb
+++ mysql_conn_test.rb
@@ -1,5 +1,5 @@
 require 'mysql'
 
-dbh = Mysql.real_connect("213.171.200.63", "fhtestuser", "entropy.0", "ERADARmain")
+dbh = Mysql.real_connect("ipaddress", "username", "password", "ERADARmain")
 puts "Server version: " + dbh.get_server_info
 dbh.close if dbh
